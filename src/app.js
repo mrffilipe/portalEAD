cf4d13373db393ab95a606f2ce35f70483687894
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2021-11-04 22:46:46 -0300

    rota /teste zueira 2

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ const indexRoutes = require('./routes/index-routes')
 const authRoutes = require('./routes/auth-routes');
 
 /* Use routes */
-app.use('/test', (req, res) => res.send('Mister seu palhaço. Hehehehheh jovi'));
+app.use('/test/:nome?', (req, res) => res.send(`${req.params.nome} seu palhaço. Hehehehheh jovi`));
 app.use(indexRoutes);
 app.use(authRoutes);
 
