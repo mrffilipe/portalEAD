01ce32e4405866a51d6ad3219cb4137fc4719794
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2021-11-11 22:57:05 -0300

    testando upload firebase

--- src/config/multer-config.js
+++ src/config/multer-config.js
@@ -1,6 +1,19 @@
 const multer = require('multer');
+const { Storage } = require('@google-cloud/storage');
 const path = require('path');
 const crypto = require('crypto');
+
+const storage = new Storage()
+
+async function uploadFile() {
+    await storage.bucket('portal-ead-dev.appspot.com').upload(path.resolve(__dirname, "..", "..", "tmp", "uploads", "c86f91968c2c494f12e542b240e1c402-nature.jpg"), {
+        destination: 'nome da minha img.jpg',
+    });
+
+    //console.log(`${filePath} uploaded to ${bucketName}`);
+}
+
+//uploadFile().catch(console.error);
 
 const storageTypes = {
     local: multer.diskStorage({
@@ -15,7 +28,10 @@ const storageTypes = {
             });
         }
     }),
-    cloud: {}
+    cloud: multer.diskStorage({
+        google: storage,
+
+    })
 };
 
 module.exports = {
