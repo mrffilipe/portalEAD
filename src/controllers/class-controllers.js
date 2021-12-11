af185a023f732aa85dc4b6421526954f8eccf1c5
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2021-12-10 23:57:16 -0300

    Documentando rotas referente a aula, e atualizando http status code do controller da aula

--- src/controllers/class-controllers.js
+++ src/controllers/class-controllers.js
@@ -6,7 +6,7 @@ exports.store = async (req, res) => {
         let { idplaylist } = req.params;
         let { title, description } = req.body;
 
-        if (await knex('playlist').where({ id: idplaylist, _idUser: id }) == 0) return res.status(500).send({ message: 'Playlist not found!' });
+        if (await knex('playlist').where({ id: idplaylist, _idUser: id }) == 0) return res.status(404).send({ message: 'Playlist not found!' });
 
         await knex('class').insert({
             playlist_id: idplaylist,
@@ -28,9 +28,9 @@ exports.index = async (req, res) => {
 
         let indexClasses = await knex('class').select('pathVideo', 'title', 'description').where({ playlist_id: idplaylist });
 
-        if (indexClasses == 0) return res.status(500).send({ message: 'Classes were not found in this playlist!' })
+        if (indexClasses == 0) return res.status(404).send({ message: 'Classes were not found in this playlist!' })
 
-        return res.status(201).send({ message: 'Classes listed!', indexClasses });
+        return res.status(200).send(indexClasses);
     } catch (error) {
         console.log(error);
         return res.status(500).send({ error: 'Classes not listed!' });
@@ -43,11 +43,11 @@ exports.update = async (req, res) => {
         let { idplaylist, idclass } = req.params;
         let { title, description } = req.body;
 
-        if (await knex('playlist').where({ id: idplaylist, _idUser: id }) == 0) return res.status(500).send({ message: 'Playlist not found!' });
+        if (await knex('playlist').where({ id: idplaylist, _idUser: id }) == 0) return res.status(404).send({ message: 'Playlist not found!' });
 
-        if (await knex('class').where({ playlist_id: idplaylist, id: idclass }).update({ 'title': title, 'description': description, 'pathVideo': 'link' }) == 0) return res.status(500).send({ message: 'No records were found with the data entered!' });
+        if (await knex('class').where({ playlist_id: idplaylist, id: idclass }).update({ 'title': title, 'description': description, 'pathVideo': 'link' }) == 0) return res.status(406).send({ message: 'No records were found with the data entered!' });
 
-        return res.status(201).send({ message: 'Class updated!' });
+        return res.status(200).send({ message: 'Class updated!' });
     } catch (error) {
         console.log(error);
         return res.status(500).send({ error: 'Class not updated!' });
@@ -59,9 +59,9 @@ exports.delete = async (req, res) => {
         let { id } = req.userData;
         let { idplaylist, idclass } = req.params;
 
-        if (await knex('playlist').where({ id: idplaylist, _idUser: id }) == 0) return res.status(500).send({ message: 'Playlist not found!' });
+        if (await knex('playlist').where({ id: idplaylist, _idUser: id }) == 0) return res.status(404).send({ message: 'Playlist not found!' });
 
-        if (await knex('class').where({ playlist_id: idplaylist, id: idclass }).del() == 0) return res.status(500).send({ message: 'No records were found with the data entered!' });
+        if (await knex('class').where({ playlist_id: idplaylist, id: idclass }).del() == 0) return res.status(406).send({ message: 'No records were found with the data entered!' });
 
         return res.status(200).send({ message: 'Class deleted!' });
     } catch (error) {
