8f9275abffc7128d2173fae3640b16c6067293ab
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2021-12-10 23:28:55 -0300

    Documentando rotas referente a playlist, e atualizando http status code do controller da playlist

--- src/controllers/playlist-controller.js
+++ src/controllers/playlist-controller.js
@@ -7,7 +7,7 @@ exports.store = async (req, res) => {
     try {
         let { id, name, lastName } = await User.findById(req.userData.id);
 
-        if (!id || id == null) return res.status(500).send({ message: 'User not found!' });
+        if (!id || id == null) return res.status(404).send({ message: 'User not found!' });
 
         let { playlistName } = req.body;
 
@@ -26,9 +26,9 @@ exports.index = async (req, res) => {
     try {
         let allPlaylist = await knex.select('name', 'nameUser').from('playlist');
 
-        if (allPlaylist == 0) return res.status(500).send({ message: 'Playlist not found!' });
+        if (allPlaylist == 0) return res.status(404).send({ message: 'Playlist not found!' });
 
-        res.status(200).send({ message: 'Playlist listed!', allPlaylist });
+        res.status(200).send(allPlaylist);
     } catch (error) {
         console.log(error);
         res.status(500).send({ error: 'Playlist not listed!' });
@@ -41,9 +41,9 @@ exports.update = async (req, res) => {
         let { id } = await req.userData;
         let { playlistName } = req.body;
 
-        if (!playlistName) return res.status(500).send({ message: 'Playlist name cannot be empty!' });
+        if (!playlistName) return res.status(406).send({ message: 'Playlist name cannot be empty!' });
 
-        if (await knex('playlist').where({ id: idplaylist, _idUser: id }).update('name', playlistName) == 0) return res.status(500).send({ message: 'No records were found with the data entered!' });
+        if (await knex('playlist').where({ id: idplaylist, _idUser: id }).update('name', playlistName) == 0) return res.status(404).send({ message: 'No records were found with the data entered!' });
 
         return res.status(200).send({ message: 'Playlist updated!' });
     } catch (error) {
@@ -57,7 +57,7 @@ exports.delete = async (req, res) => {
         let { idplaylist } = req.params;
         let { id } = await req.userData;
 
-        if (await knex('playlist').where({ id: idplaylist, _idUser: id }).del() == 0) return res.status(500).send({ message: 'No records were found with the data entered!' });
+        if (await knex('playlist').where({ id: idplaylist, _idUser: id }).del() == 0) return res.status(404).send({ message: 'No records were found with the data entered!' });
 
         return res.status(200).send({ message: 'Playlist deleted!' });
     } catch (error) {
