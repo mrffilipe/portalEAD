9856485e73c2a0b2ad9dbd0546a3b596f80d72ee
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2021-12-11 18:18:00 -0300

    Habilitando o cors 4

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ require('./config/db-sql');
 const cors = require('cors')
 const app = express();
 
+app.use(cors());
+
 app.use(
     json({
         limit: '5mb'
@@ -21,7 +23,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    app.use(cors());
+    
     next();
 });
 
