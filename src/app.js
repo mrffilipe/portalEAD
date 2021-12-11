0c5781cb8e176583a18574d3820c481a0fe84842
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2021-12-11 19:06:44 -0300

    Habilitando o cors 5

--- src/app.js
+++ src/app.js
@@ -19,13 +19,13 @@ app.use(
 );
 
 // Habilita o CORS
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     
     next();
-});
+});*/
 
 app.use(
     "/files",
